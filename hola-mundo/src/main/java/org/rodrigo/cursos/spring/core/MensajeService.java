680ea7067180f16e1cfdcb0314de778a73680d59
Rodrigo <rhodfra@gmail.com>
2020-10-24 11:59:10 -0500

    udpated repository with workspace linter configurations

--- hola-mundo/src/main/java/org/rodrigo/cursos/spring/core/MensajeService.java
+++ hola-mundo/src/main/java/org/rodrigo/cursos/spring/core/MensajeService.java
@@ -1,17 +1,27 @@
 package org.rodrigo.cursos.spring.core;
 
+/**
+ * TODO [Add class documentation]
+ */
 public class MensajeService {
-	
-	// Atributo que se inyectado por Spring
-	private String mensaje;
-	
-	public String decoraMensaje() {
-		return "ยก" + mensaje + "!";
-	}
 
-	public void setMensaje(String mensaje) {
-		this.mensaje = mensaje;
-	}
+  // Atributo que se inyectado por Spring
+  private String mensaje;
 
-	
+  /**
+   * TODO [Add method documentation]
+   * @return
+   */
+  public String decoraMensaje() {
+    return "ก" + mensaje + "!";
+  }
+
+  /**
+   * TODO [Add method documentation]
+   * @param mensaje
+   */
+  public void setMensaje(String mensaje) {
+    this.mensaje = mensaje;
+  }
+
 }
--- hola-mundo/src/test/java/com/rodrigo/curso/spring/core/test/HolaMundoJunit5.java
+++ hola-mundo/src/test/java/com/rodrigo/curso/spring/core/test/HolaMundoJunit5.java
@@ -9,40 +9,43 @@ import org.rodrigo.cursos.spring.core.MensajeService;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+/**
+ * TODO [Add class documentation]
+ */
 public class HolaMundoJunit5 {
-	
-	private static MensajeService mensajeService;
-	
-	private static ApplicationContext appContext;
-	
-	/**
-	 * Crear un spring container e inicializar el servicio <code>mensajeService</code>
-	 */
-	@BeforeAll
-	public static void beforeAll() {
-		appContext = new ClassPathXmlApplicationContext("applicationContext.xml");
-		mensajeService = (MensajeService)appContext.getBean("mensajeService");
-	}
-	
-	/**
-	 * Cierra el application context
-	 */
-	@AfterAll
-	public static void afterAll() {
-		((ClassPathXmlApplicationContext)appContext).close();
-	}
-	
-	/**
-	 * Prueba unitaria que verifica el funcionamiento del servicio 
-	 * {@link MensajeService}
-	 */
-	@Test
-	public void holaMundo() {
-		
-		String mensajeDecorado;
-		mensajeDecorado = mensajeService.decoraMensaje();
-		
-		assertEquals("Â¡Hola mundo!",mensajeDecorado,"Los mensajes no coinciden.");
-	}
+
+  private static MensajeService mensajeService;
+
+  private static ApplicationContext appContext;
+
+  /**
+   * Crear un spring container e inicializar el servicio <code>mensajeService</code>
+   */
+  @BeforeAll
+  public static void beforeAll() {
+    appContext = new ClassPathXmlApplicationContext("applicationContext.xml");
+    mensajeService = (MensajeService) appContext.getBean("mensajeService");
+  }
+
+  /**
+   * Cierra el application context
+   */
+  @AfterAll
+  public static void afterAll() {
+    ((ClassPathXmlApplicationContext) appContext).close();
+  }
+
+  /**
+   * Prueba unitaria que verifica el funcionamiento del servicio
+   * {@link MensajeService}
+   */
+  @Test
+  public void holaMundo() {
+
+    String mensajeDecorado;
+    mensajeDecorado = mensajeService.decoraMensaje();
+
+    assertEquals("¡Hola mundo!", mensajeDecorado, "Los mensajes no coinciden.");
+  }
 
 }
